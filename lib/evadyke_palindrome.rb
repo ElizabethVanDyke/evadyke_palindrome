cc5edf227806719933787e6f6f67fe4e32839d0b
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-06-18 14:35:21 +0000

    Add integer palindrome detection

--- lib/evadyke_palindrome.rb
+++ lib/evadyke_palindrome.rb
@@ -1,12 +1,28 @@
 require "evadyke_palindrome/version"
-class String
+
+module EvandykePalindrome
+  # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
+
+    # Returns content for palindrome testing.
     def processed_content
-     self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z\d]/i).join.downcase
     end
 end
 
+
+class String
+ include EvandykePalindrome
+end
+
+class Integer
+  include EvandykePalindrome
+end
